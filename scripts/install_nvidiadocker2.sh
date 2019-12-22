59595f1896ab004e61a683aac0ae10d4c53e1cad
Mike Hollinger <mchollin@us.ibm.com>
2019-12-22 10:48:57 -0600

    Reduce apt and dpkg output further

--- scripts/install_nvidiadocker2.sh
+++ scripts/install_nvidiadocker2.sh
@@ -5,7 +5,7 @@ curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
   sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-sudo apt-get -qq update
-sudo apt-get -qq -y install nvidia-docker2
+sudo apt-get -qq -o Dpkg::Use-Pty=0 update
+sudo apt-get -qq -o Dpkg::Use-Pty=0 -y install nvidia-docker2
 sudo pkill -SIGHUP dockerd
 echo "SUCCESS: nvidia-docker2 started!"
