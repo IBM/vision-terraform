5bed13f42b189b625840079d9487b0fd606f1e18
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2020-02-27 09:27:28 +0530

    Removed trial check

--- scripts/install_vision.sh
+++ scripts/install_vision.sh
@@ -17,7 +17,7 @@ BASEDIR="$(dirname "$0")"
 # shellcheck disable=SC1090
 source ${BASEDIR}/env.sh
 
-dpkg -i ${RAMDISK}/*trial*.deb
+dpkg -i ${RAMDISK}/*.deb
 LOGFILE=/opt/powerai-vision/install_vision.log
 echo "INFO: Loading PowerAI Vision Docker images. This will take several minutes..."
 time /opt/powerai-vision/bin/load_images.sh -f ${RAMDISK}/powerai-vision-images-*.tar &>${LOGFILE}
