057beeee0e809cfd046c21f27552457083d57793
Mike Hollinger <mchollin@us.ibm.com>
2020-03-25 12:58:40 -0500

    Update patch_postinstall.sh

--- scripts/patch_postinstall.sh
+++ scripts/patch_postinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-# Copyright 2019. IBM All Rights Reserved.
+# Copyright 2020. IBM All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
